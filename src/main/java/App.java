b22c321a7d839b061e3705de8297006ee1d5b15a
Annie Sonna <nsab42@yahoo.com>
2016-09-11 18:29:43 -0700

    Updating first attempt at coding assuming customer provide their budget and in return is matched with available event packages.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,34 @@
-
 import java.io.Console;
 import java.util.List;
 
 public class App {
   public static void main(String[] args) {
+    Console myConsole = System.console();
 
     System.out.println("I'm building an event application!");
-    
-  }
+
+    Event firstEvent = new Event(50, "CD", "lightSnack", "Water", 0, 0);
+    Event secondEvent = new Event(250, "DJ", "Plated", "Wine", 1, 0);
+    Event thirdEvent = new Event(500, "LifeBand", "Buffet", "Wine", 4, 0);
+    Event fourEvent = new Event(75, "Lifeband", "ThreeCourse", "FullBar", 3, 0);
+    Event fifthEvent = new Event(200, "DJ", "Buffet", "SoftDrinks", 2, 0);
+
+    Event[] allEvents = {firstEvent, secondEvent, thirdEvent, fourEvent, fifthEvent};
+
+    System.out.println("What is the maximum budget you have for your event?");
+    int userMaxBudget = Integer.parseInt(myConsole.readLine());
+    System.out.println("Alright, here's are some packages we have in your price range of " + userMaxBudget + ".");
+
+    for ( Event individualEvent : allEvents ) {
+     if (individualEvent.eventPackage(userMaxBudget)){
+       System.out.println( "----------------------" );
+       System.out.println( individualEvent.mNumberOfGuests );
+       System.out.println( individualEvent.mMusic );
+       System.out.println( individualEvent.mFoodType );
+       System.out.println( individualEvent.mDrinkType );
+       System.out.println( individualEvent.mNumberOfEntertainers );
+       System.out.println( individualEvent.mTotalCost );
+     }
+   }
+ }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -3,13 +3,26 @@ import java.util.List;
 import java.io.Console;
 import java.util.Arrays;
 
-public class Event{
+class Event{
   // Console myConsole = System.console();
 
-  // public int mNumberOfGuests
-  // public String mMusic
-  // public String mFoodType
-  // public String mDrinkType
-  // public String mEntertainment
-  // public int mTotalCost
+  public int mNumberOfGuests;
+  public String mMusic;
+  public String mFoodType;
+  public String mDrinkType;
+  public String mNumberOfEntertainers;
+  public int mTotalCost;
+
+  public Event(int numberOfGuests, String music, String foodType, String drinkType, int numberOfntertainers, int totalCost) {
+    mNumberOfGuests = numberOfGuests;
+    mMusic = music;
+    mFoodType = foodType;
+    mDrinkType = drinkType;
+    mNumberOfEntertainers = numberOfntertainers;
+    mTotalCost = totalCost;
+  }
+
+  public boolean eventPackage(int maxBudget){
+    return (mTotalCost <= maxBudget);
+  }
 }
